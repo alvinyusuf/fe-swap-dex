3a40a32e93db82ea6e0f2d3746f449102ea9c1d5
Alvin Yusuf Riziq <alvinysfriz1@std.unissula.ac.id>
2024-12-01 00:14:18 +0700

    ADD: MVP swap page

---
+++ src/components/tokens-button.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from './ui/button'
+
+export default function TokensButton() {
+  return (
+    <Button className='bg-[#00ADB5] text-white rounded-xl'>ETH</Button>
+  )
+}
